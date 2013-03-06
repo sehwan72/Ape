807c38d8595685fb7a69b2b8f0ff0f27143afd8a
Tim <tim@abyss.(none)>
2013-03-05 23:16:30 -0500

    Added Process skeleton

---
+++ src/Process.h
@@ -0,0 +1,24 @@
+#ifndef __PROCESS_
+#define __PROCESS_
+
+#include <iostream>
+
+class Process 
+{
+    private:
+        // Private data members and functions
+
+    public:
+        // Public data members and functions
+        Process();
+
+        // Process Monitoring
+        
+
+
+        // Process Management
+        
+
+}
+
+#endif
