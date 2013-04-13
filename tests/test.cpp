086335b343baca8bccfb7ca56f7cbc2b67fd6ab2
Tim Thornton <igorii@localhost.(none)>
2013-04-12 23:13:14 -0400

    Added system start time to Sys as btime

--- tests/test.cpp
+++ tests/test.cpp
@@ -27,16 +27,16 @@ int main()
     //return 0;
 
 
-    while (1) {
+    //while (1) {
         printf("\n");
         a.update();
-        a.sort(PPID);
+        a.sort(CPU);
         a.printProcesses(CPU);
         usleep(500 * 1000);
         a.update();
         usleep(500 * 1000);
         system("clear");
-    }
+    //}
     
 
     //removeTest();
